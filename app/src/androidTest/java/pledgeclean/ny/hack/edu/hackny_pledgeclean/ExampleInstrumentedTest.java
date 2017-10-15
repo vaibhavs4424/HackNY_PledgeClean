1bde256f132fc2179b2d2ed89987f3fd71d42d3c
vaibhavshukla <vaibhavs4424@gmail.com>
2017-10-14 21:48:54 -0400

    Initial Push

---
+++ app/src/androidTest/java/pledgeclean/ny/hack/edu/hackny_pledgeclean/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package pledgeclean.ny.hack.edu.hackny_pledgeclean;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("pledgeclean.ny.hack.edu.hackny_pledgeclean", appContext.getPackageName());
+    }
+}
